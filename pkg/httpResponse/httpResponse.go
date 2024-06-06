8ca619b88309a7701d1a8a1012214f387832dbde
Diego Ramos <diegorbrito9@gmail.com>
2024-06-06 14:16:02 -0300

    chore: removendo função de respostas para erro

--- pkg/httpResponse/httpResponse.go
+++ pkg/httpResponse/httpResponse.go
@@ -2,7 +2,6 @@ package httpResponse
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
@@ -10,15 +9,4 @@ func NewJsonResponse(w http.ResponseWriter, status int, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(data)
-}
-
-func NewErrorResponse(w http.ResponseWriter, status int, err error) {
-	type errorMessage struct {
-		Error string `json:"error"`
-	}
-
-	fmt.Printf("\nerror: %v\n", err)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(errorMessage{Error: err.Error()})
 }
