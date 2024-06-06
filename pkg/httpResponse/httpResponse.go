2cd4601f4bf3ad79353769dbaf9c14f16fdabe11
Diego Ramos <diegorbrito9@gmail.com>
2024-06-06 15:25:13 -0300

    chore: criando função para retornar erro

--- pkg/httpResponse/httpResponse.go
+++ pkg/httpResponse/httpResponse.go
@@ -5,8 +5,23 @@ import (
 	"net/http"
 )
 
+type errResponse struct {
+	Error string `json:"error"`
+}
+
 func NewJsonResponse(w http.ResponseWriter, status int, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(data)
+}
+
+func NewErrorResponse(w http.ResponseWriter, status int, err error) {
+
+	r := errResponse{
+		Error: err.Error(),
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(r)
 }
