eabc86934444480cc0721fe14007aadf61d3dbee
Diego Ramos <diegorbrito9@gmail.com>
2024-06-06 20:17:49 -0300

    chore: adicionando 'secret' do jwt no arquivo '.env'

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -8,19 +8,22 @@ import (
 type EnvironmentVariables struct {
 	ServerPort  string
 	DatabaseUrl string
+	JwtSecret   string
 }
 
 func GetEnvironmentVariables() *EnvironmentVariables {
 	serverPortEnv := os.Getenv("PORT")
 	databaseUrlEnv := os.Getenv("DATABASE_URL")
+	jwtSecret := os.Getenv("JWT_SECRET")
 
 	// valides if environments variables was set
-	if serverPortEnv == "" || databaseUrlEnv == "" {
+	if serverPortEnv == "" || databaseUrlEnv == "" || jwtSecret == "" {
 		panic("All environment variable must be defined on '.env' file")
 	}
 
 	return &EnvironmentVariables{
 		ServerPort:  fmt.Sprintf(":%v", serverPortEnv),
 		DatabaseUrl: fmt.Sprintf("%s?sslmode=disable", databaseUrlEnv),
+		JwtSecret:   jwtSecret,
 	}
 }
