0b903e6842320c61620226278fd8a1660e396f42
Diego Ramos <diegorbrito9@gmail.com>
2024-06-06 15:10:05 -0300

    chore: removendo aspas a mais

--- models/user.go
+++ models/user.go
@@ -1,13 +1,9 @@
 package models
 
 type User struct {
-	ID        int    `json:"id" "`
+	Id        int    `json:"id"`
 	Name      string `json:"name" validate:"required"`
 	LastName  string `json:"lastName" validate:"required"`
 	TaxId     string `json:"taxId" validate:"required"`
 	BirthDate string `json:"birthDate" validate:"required"`
-}
-
-func NewUser(name, lastName, taxId, birthDate string) User {
-	return User{Name: name, LastName: lastName, TaxId: taxId, BirthDate: birthDate}
 }
